3e0d3806788078a63f2009e2e754253b280f0319
marydomashneva <mary.domashneva@gmail.com>
2018-05-21 14:32:32 +0100

    Passing header method test

--- lib/print_transactions.rb
+++ lib/print_transactions.rb
@@ -1,5 +1,6 @@
-class Print
+class Printer
 
-  def print_header
+  def print_header(header = 'date || credit || debit || balance')
+    @header = header
   end
 end
--- spec/print_transactions_spec.rb
+++ spec/print_transactions_spec.rb
@@ -1,8 +1,15 @@
 require './lib/bank.rb'
 require './lib/print_transactions.rb'
 
-describe Print do
+describe Printer do
   it 'responds to print_header' do
     expect(subject).to respond_to :print_header
+  end
+
+  context 'when printing string header' do
+    it 'prints header' do
+      header = 'date || credit || debit || balance'
+      expect(subject.print_header(header)).to equal(header)
+    end
   end
 end
