d642207769bfc99cd340138dec897a93ecc8e4c4
marydomashneva <mary.domashneva@gmail.com>
2018-05-21 17:41:00 +0100

    Refactoring Printer class, define date modifier as a private method

--- lib/print_transactions.rb
+++ lib/print_transactions.rb
@@ -1,19 +1,20 @@
+require 'date'
+
 class Printer
   def return_string_header(header = 'date || credit || debit || balance')
-    @header = header
+    header
   end
 
   def return_string_transactions(transactions)
-    @string = ''
-    @transactions = transactions.sort.reverse.to_h
-    @transactions.each do |date, statement|
+    string = ''
+    all_transactions = hash_reverse(transactions)
+    all_transactions.each do |date, statement|
       if statement[1] == 'debit'
-        then @string += "#{date.strftime('%m/%d/%Y')} || || #{to_float(statement[0])} || #{to_float(statement[2])} \n"
-      else @string += "#{date.strftime('%m/%d/%Y')} || #{to_float(statement[0])} || || #{to_float(statement[2])} \n"
+        then string += "#{date_modif(date)} || || #{to_float(statement[0])} || #{to_float(statement[2])} \n"
+      else string += "#{date_modif(date)} || #{to_float(statement[0])} || || #{to_float(statement[2])} \n"
       end
     end
-    p @string.chomp(' ')
-    @string.chomp(' ')
+    string.chomp(' ')
   end
 
   def print_statement(header = 'date || credit || debit || balance', transactions)
@@ -25,5 +26,13 @@ class Printer
 
   def to_float(number)
     format('%.2f', number)
+  end
+
+  def date_modif(date_string)
+    Date.parse(date_string).strftime('%d/%m/%Y')
+  end
+
+  def hash_reverse(hash)
+    hash.sort.reverse.to_h
   end
 end
