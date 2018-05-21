b62f15c226a31eee402a6772110d949de9023038
marydomashneva <mary.domashneva@gmail.com>
2018-05-21 16:31:37 +0100

    Print method

--- lib/print_transactions.rb
+++ lib/print_transactions.rb
@@ -5,13 +5,19 @@ class Printer
 
   def return_string_transactions(transactions)
     @string = ''
-    transactions.each do |date, transaction|
-      if transaction[1] == 'debit'
-        then @string += "#{date} || || #{to_float(transaction[0])} || #{to_float(transaction[2])} "
-      else @string += "#{date} || #{to_float(transaction[0])} || || #{to_float(transaction[2])} "
+    transactions.each do |date, statement|
+      if statement[1] == 'debit'
+        then @string += "#{date} || || #{to_float(statement[0])} || #{to_float(statement[2])} \n"
+      else @string += "#{date} || #{to_float(statement[0])} || || #{to_float(statement[2])} \n"
       end
     end
+    p @string.chomp(' ')
     @string.chomp(' ')
+  end
+
+  def print_statement(header = 'date || credit || debit || balance', transactions = {})
+    p return_string_header(header)
+    p return_string_transactions(transactions)
   end
 
   private
